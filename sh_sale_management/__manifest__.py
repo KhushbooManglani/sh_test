2d1b43144db1febb6acb0ae1352a9e7d32e0cb32
Khushboo Manglani <khushboomanglani48@gmail.com>
2024-02-23 23:09:06 +0530

    [add] custom sale module

---
+++ sh_sale_management/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Sales Management',
+    'version': '17.0.1.0.1',
+    'category': 'Extra Tools',
+    'summary': 'Sales internal machinery',
+    'description': """
+This module contains all the common features of Sales Management and eCommerce.
+    """,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ sh_sale_management/models/sale_order.py
@@ -0,0 +1,5 @@
+from odoo import api, fields, models, _, tools
+
+class Sale_Order(models.Model):
+    _name = "sh.sale.management"
+    _description = "Sale Management"
